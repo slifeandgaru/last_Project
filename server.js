c55dfbcd8ba5651aafa7215b522ab9dfa3e2b5c4
Pham Tan Khang <dooangiang2612@gmail.com>
2021-06-29 20:17:21 +0700

    khang update

--- server.js
+++ server.js
@@ -10,7 +10,13 @@ app.use(bodyParser.json())
 app.use('/public', express.static(path.join(__dirname, "/public")))
 //html
 app.get('/floda', (req,res) => {
-    res.sendFile(path.join(__dirname,'./public/html/index.html'))
+    res.sendFile(path.join(__dirname,'./public/html/home.html'))
+})
+app.get('/floda/gioi-thieu', (req,res) => {
+    res.sendFile(path.join(__dirname,'./public/html/gioithieu.html'))
+})
+app.get('/floda/lien-he', (req,res) => {
+    res.sendFile(path.join(__dirname,'./public/html/lienhe.html'))
 })
 
 app.listen(3000)
