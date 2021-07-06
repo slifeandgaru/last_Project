5abfeb873d8bdea4e4747daa11ecc7e5705c2aab
Pham Tan Khang <dooangiang2612@gmail.com>
2021-07-06 17:04:15 +0700

    khang update

---
+++ public/js/store.js
@@ -0,0 +1,11 @@
+var number = 0;
+function list11() {
+  console.log('ádas');
+  if (number == 1) {
+    $("#list111").hide();
+    number = 0;
+  } else {
+    number++;
+    $("#list111").show();
+  }
+}
--- test.js
+++ test.js
@@ -1,1 +1,3 @@
-
+function aa(){
+    $("#listtt").show();
+}
